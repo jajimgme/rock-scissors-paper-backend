82bd248692da8187e4d7e867eb78738aab97b4c6
jjimenez <jjimenez@iti.es>
2022-08-19 19:32:03 +0200

    Added AI Classes.

---
+++ src/main/java/com/aviatar/rock_scissors_paper/domain/game_implementations/ai/RockPaperScissorsAI.java
@@ -0,0 +1,2 @@
+package com.aviatar.rock_scissors_paper.domain.game_implementations.ai;public class RockPaperScissorsAI {
+}
--- src/main/java/com/aviatar/rock_scissors_paper/domain/model/ClassicPlay.java
+++
@@ -1,9 +0,0 @@
-package com.aviatar.rock_scissors_paper.domain.model;
-
-import lombok.Data;
-
-@Data
-public class ClassicPlay implements Play{
-    public Pick player1Pick;
-    public Pick player2Pick;
-}
