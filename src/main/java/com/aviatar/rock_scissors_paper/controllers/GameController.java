20e252e7ee9a7a1df8a76c0bc05336137a2d0440
jjimenez <jjimenez@iti.es>
2022-08-19 19:37:07 +0200

    Setup controller paths.

--- src/main/java/com/aviatar/rock_scissors_paper/controllers/GameController.java
+++ src/main/java/com/aviatar/rock_scissors_paper/controllers/GameController.java
@@ -12,30 +12,20 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController("/game")
+@RestController()
+@RequestMapping(value = "/game", produces = {"application/json"})
 public class GameController {
     @Autowired
     private IGameService gameService;
-// TODO Instanciar el servicio de manera que permita jugar. 
     public GameController(GameService gameService){
         this.gameService = gameService;
     }
 
-//    @PostMapping()
-//    public ResponseEntity<GameDto> createRockPaperScissorsGame(CreateGameDto gameDto){
-////        try {
-////            Game game = gameService.createGame(gameDto);
-////            GameDto dto = new GameDto();
-////            return new ResponseEntity<>(dto, HttpStatus.OK);
-////        } catch (ExecutionControl.NotImplementedException e) {
-////            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-////        }
-
-//    }
     @GetMapping
     public ResponseEntity<List<GameDescription>> getAvailableGames(){
     return new ResponseEntity<>(gameService.getGames(), HttpStatus.OK);
--- src/main/java/com/aviatar/rock_scissors_paper/controllers/RockPaperScissorController.java
+++ src/main/java/com/aviatar/rock_scissors_paper/controllers/RockPaperScissorController.java
@@ -7,9 +7,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/rock-paper-scissor")
+@RestController()
+@RequestMapping(value = "/rock-paper-scissor", produces = {"application/json"})
 public class RockPaperScissorController {
 
     private RockPaperScissorsGameService gameService;
