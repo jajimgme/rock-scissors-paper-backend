8a0ccd66c3b4a55423c3aab4052e750ceea45479
jjimenez <jjimenez@iti.es>
2022-08-23 13:29:05 +0200

    Introduce interface.

--- src/main/java/com/test/rock_scissors_paper/repository/GameRepository.java
+++ src/main/java/com/test/rock_scissors_paper/repository/GameRepository.java
@@ -2,40 +2,11 @@ package com.test.rock_scissors_paper.repository;
 
 import com.test.rock_scissors_paper.domain.model.GameDescription;
 import com.test.rock_scissors_paper.domain.model.GameType;
-import org.springframework.stereotype.Repository;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 
-@Repository
-public class GameRepository {
-    List<GameDescription> games;
+public interface GameRepository {
+    List<GameDescription> getAvailableGames();
 
-    public GameRepository() {
-        GameDescription game = new GameDescription();
-        game.setNumberOfPlayers(2);
-        game.setDescriptionOfTheRules("This is the classic instance of the rock, paper, scissors game.");
-        game.setGameType(new GameType(1, "Classic picks rock paper scissors"));
-        game.setPlayingEndpoint("/rock-paper-scissor/");
-        games = Arrays.asList(game);
-    }
-
-    public List<GameDescription> getAvailableGames() {
-        return games;
-    }
-
-    public GameDescription getDescriptionOfGameType(GameType g) {
-        return findGame(g);
-    }
-
-    private GameDescription findGame(GameType gameType) {
-        Optional<GameDescription> first = this.games.stream().filter(g -> g.getGameType().getId() == gameType.getId()).findFirst();
-        if (first.isPresent()) {
-            return first.get();
-        } else {
-            throw new RuntimeException("That gametype is not available in this service");
-        }
-    }
-
+    GameDescription getDescriptionOfGameType(GameType g);
 }
---
+++ src/main/java/com/test/rock_scissors_paper/repository/LocalTestGameRepository.java
@@ -0,0 +1,43 @@
+package com.test.rock_scissors_paper.repository;
+
+import com.test.rock_scissors_paper.domain.model.GameDescription;
+import com.test.rock_scissors_paper.domain.model.GameType;
+import org.springframework.stereotype.Repository;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class LocalTestGameRepository implements GameRepository {
+    List<GameDescription> games;
+
+    public LocalTestGameRepository() {
+        GameDescription game = new GameDescription();
+        game.setNumberOfPlayers(2);
+        game.setDescriptionOfTheRules("This is the classic instance of the rock, paper, scissors game.");
+        game.setGameType(new GameType(1, "Classic picks rock paper scissors"));
+        game.setPlayingEndpoint("/rock-paper-scissor/");
+        games = Arrays.asList(game);
+    }
+
+    @Override
+    public List<GameDescription> getAvailableGames() {
+        return games;
+    }
+
+    @Override
+    public GameDescription getDescriptionOfGameType(GameType g) {
+        return findGame(g);
+    }
+
+    private GameDescription findGame(GameType gameType) {
+        Optional<GameDescription> first = this.games.stream().filter(g -> g.getGameType().getId() == gameType.getId()).findFirst();
+        if (first.isPresent()) {
+            return first.get();
+        } else {
+            throw new RuntimeException("That gametype is not available in this service");
+        }
+    }
+
+}
